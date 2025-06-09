c5d8a8dac348722a3c1fb39683cd03749315669f
nachum <nahum0583@gmail.com>
2025-06-09 11:36:19 +0300

    start project + sql

---
+++ Malshinon.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE IF NOT EXISTS Malshinon;
+
+USE Malshinon;
+
+CREATE TABLE IF NOT EXISTS People (
+    `ID` INT PRIMARY KEY AUTO_INCREMENT,
+    `First_Name` VARCHAR(10) UNIQUE NOT NULL,
+    `Last_Name` VARCHAR(10) UNIQUE NOT NULL,
+    `Secret_Code` VARCHAR(20) UNIQUE NOT NULL,
+    `Type` ENUM ('reporter','target','both','potential_agent') DEFAULT 'reporter',
+    `Num_Reports` INT DEFAULT 0 CHECK(`Type` = 'reporter'),
+    `Num_Mentions` INT DEFAULT 0 CHECK(`Type` = 'target')
+);
+
+CREATE TABLE IF NOT EXISTS IntelReports(
+    `ID` INT PRIMARY KEY AUTO_INCREMENT,
+    `Reporter_Id` INT,
+    FOREIGN KEY (`Reporter_Id`) REFERENCES `People`(`ID`),
+    `Target_Id` INT,
+    FOREIGN KEY (`Target_Id`) REFERENCES `People`(`ID`),
+    `Text` TEXT,
+    `Timestamp` DATETIME DEFAULT NOW()
+    
+);
+
